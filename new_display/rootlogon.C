3361253ccdf4a2ed8be31a6624df10c3fdaa40ea
Chris Walter <chris.walter@duke.edu>
2010-06-29 01:28:58 +0000

    Moving this code into rootdisplay area

--- new_display/rootlogon.C
+++
@@ -1,7 +0,0 @@
-// rootlogon.C  
-{
-    printf("rootlogon.C: Loading WCSimRoot Library...\n");
-    gSystem->Load("libWCSimRoot.so");
-    printf("rootlogon.C: Loading WCMap Library...\n");
-    gSystem->Load("libWCMap.so");
-}
