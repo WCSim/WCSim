ab80a02e81e945d983690e88249a0a11182656c7
David Webber <dwebber@hep.wisc.edu>
2010-07-08 22:13:57 +0000

    Correction: WCSim does not throw an exception for detector construction when the physics list is QGSP_BERT.  Maybe something wrong with LBE physics list

--- WCSim.cc
+++ WCSim.cc
@@ -80,7 +80,6 @@ int main(int argc,char** argv)
   runManager->SetUserAction(myGeneratorAction);
 
 
-
   WCSimRunAction* myRunAction = new WCSimRunAction(WCSimdetector);
   runManager->SetUserAction(myRunAction);
 
