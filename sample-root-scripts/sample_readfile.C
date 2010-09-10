7184a65572616c738aa6fc266855ec2f0db7ef48
Chris Walter <chris.walter@duke.edu>
2010-09-10 21:16:34 +0000

    Modify thie sample_readfile to correctly process and printout raw photon hits.  Also make clear exactly what the varaibles mean in the comments.
    Note,, by default this only prints out the first 10 tubes.
    
    --This line, and those
    below, will be ignored--
    
    M    sample_readfile.C

--- sample-root-scripts/sample_readfile.C
+++ sample-root-scripts/sample_readfile.C
@@ -2,7 +2,7 @@
 {
   // Clear global scope
   gROOT->Reset();
-
+  
   gStyle->SetOptStat(0);
   gStyle->SetCanvasColor(0);
   gStyle->SetTitleColor(1);
@@ -39,7 +39,6 @@
   // Open the file
   TFile file("../wcsim.root");
   
-  
   // Get the a pointer to the tree from the file
   TTree *tree = (TTree*)file->Get("wcsimT");
   
@@ -62,109 +61,141 @@
   // and always exists.
   WCSimRootTrigger* wcsimrootevent;
 
- TH1F *h1 = new TH1F("PMT Hits on 1.5 GeV Muon in Geant4", "PMT Hits on 1.5 GeV Muon in Geant4", 3000, 3000, 9000);
-
-
+  TH1F *h1 = new TH1F("PMT Hits", "PMT Hits on 1.5 GeV Muon in Geant4", 100, 1000, 9000);
+  
   // Now loop over events
-  int ev;
-  for (ev=0;ev<nevent; ev++)
+  for (int ev=0; ev<nevent; ev++)
+  {
+    // Read the event from the tree into the WCSimRootEvent instance
+    tree->GetEntry(ev);      
+    wcsimrootevent = wcsimrootsuperevent->GetTrigger(0);
+    printf("********************************************************");
+    printf("Evt, date %d %d\n", wcsimrootevent->GetHeader()->GetEvtNum(),
+	   wcsimrootevent->GetHeader()->GetDate());
+    printf("Mode %d\n", wcsimrootevent->GetMode());
+    printf("Number of subevents %d\n",
+	   wcsimrootsuperevent->GetNumberOfSubEvents());
+    
+    printf("Vtxvol %d\n", wcsimrootevent->GetVtxvol());
+    printf("Vtx %f %f %f\n", wcsimrootevent->GetVtx(0),
+	   wcsimrootevent->GetVtx(1),wcsimrootevent->GetVtx(2));
+    printf("Jmu %d\n", wcsimrootevent->GetJmu());
+    printf("Npar %d\n", wcsimrootevent->GetNpar());
+    printf("Ntrack %d\n", wcsimrootevent->GetNtrack());
+    
+    // Now read the tracks in the event
+    
+    // Get the number of tracks
+    int ntrack = wcsimrootevent->GetNtrack();
+    printf("ntracks=%d\n",ntrack);
+    
+    int i;
+    // Loop through elements in the TClonesArray of WCSimTracks
+    for (i=0; i<ntrack; i++)
     {
-      // Read the event from the tree into the WCSimRootEvent instance
-      tree->GetEntry(ev);      
-      wcsimrootevent = wcsimrootsuperevent->GetTrigger(0);
-	printf("********************************************************");
-      printf("Evt, date %d %d\n", wcsimrootevent->GetHeader()->GetEvtNum(),
-	     wcsimrootevent->GetHeader()->GetDate());
-      printf("Mode %d\n", wcsimrootevent->GetMode());
-      printf("Number of subevents %d\n",
-	     wcsimrootsuperevent->GetNumberOfSubEvents());
-
-      printf("Vtxvol %d\n", wcsimrootevent->GetVtxvol());
-      printf("Vtx %f %f %f\n", wcsimrootevent->GetVtx(0),
-	     wcsimrootevent->GetVtx(1),wcsimrootevent->GetVtx(2));
-      printf("Jmu %d\n", wcsimrootevent->GetJmu());
-      printf("Npar %d\n", wcsimrootevent->GetNpar());
-      printf("Ntrack %d\n", wcsimrootevent->GetNtrack());
-
-      // Now read the tracks in the event
-
-      // Get the number of tracks
-      int ntrack = wcsimrootevent->GetNtrack();
-      printf("ntracks=%d\n",ntrack);
-
-      int i;
-      // Loop through elements in the TClonesArray of WCSimTracks
-      for (i=0;i<ntrack;i++){
-	
-	TObject *element = (wcsimrootevent->GetTracks())->At(i);
-
-	WCSimRootTrack *wcsimroottrack = dynamic_cast<WCSimRootTrack*>(element);
-	printf("Track ipnu: %d\n",wcsimroottrack->GetIpnu());
-	printf("Track parent ID: %d\n",wcsimroottrack->GetParenttype());
-	int j;
-		for (j=0;j<3;j++)
-		  {
-		    printf("Track dir: %d %f\n",j, wcsimroottrack->GetDir(j));
-		  }
-      }  // End of loop over tracks
+      TObject *element = (wcsimrootevent->GetTracks())->At(i);
       
-      // Now look at the Cherenkov hits
+      WCSimRootTrack *wcsimroottrack = dynamic_cast<WCSimRootTrack*>(element);
+      printf("Track ipnu: %d\n",wcsimroottrack->GetIpnu());
+      printf("Track parent ID: %d\n",wcsimroottrack->GetParenttype());
+      
+      for (int j=0; j<3; j++)
+	printf("Track dir: %d %f\n",j, wcsimroottrack->GetDir(j));
+      
+    }  // End of loop over tracks
+    
+    // Now look at the Cherenkov hits
+    
+    // Get the number of Cherenkov hits.
+    // Note... this is *NOT* the number of photons that hit tubes.
+    // It is the number of tubes hit with Cherenkov photons.
+    // The number of digitized tubes will be smaller because of the threshold.
+    // Each hit "raw" tube has several photon hits.  The times are recorded.
+    // See http://nwg.phy.bnl.gov/DDRD/cgi-bin/private/ShowDocument?docid=245
+    // for more information on the structure of the root file.
+    //  
+    // The following code prints out the hit times for the first 10 tubes and also
+    // adds up the total pe.
+    // 
+    // For digitized info (one time/charge tube after a trigger) use
+    // the digitized information.
+    //
 
-      // Get the number of Cherenkov hits
-		int ncherenkovhits = wcsimrootevent->GetNcherenkovhits();
-		int ncherenkovdigihits = wcsimrootevent->GetNcherenkovdigihits(); //GetNcherenkovhits();
-		h1.Fill(ncherenkovdigihits);
-		printf("node id: %i\n", ev);
-	   printf("Ncherenkovhits %d\n", ncherenkovhits);
-	   printf("Ncherenkovdigihits %d\n", ncherenkovdigihits);
+    int ncherenkovhits     = wcsimrootevent->GetNcherenkovhits();
+    int ncherenkovdigihits = wcsimrootevent->GetNcherenkovdigihits(); 
+    
+    h1->Fill(ncherenkovdigihits);
+    printf("node id: %i\n", ev);
+    printf("Ncherenkovhits %d\n",     ncherenkovhits);
+    printf("Ncherenkovdigihits %d\n", ncherenkovdigihits);
 
-      // Loop through elements in the TClonesArray of WCSimRootCherenkovHits
+    cout << "RAW HITS:" << endl;
 
-      /*      for (i=0;i<(ncherenkovhits>4 ? 4 : ncherenkovhits);i++){
+    // Grab the big arrays of times and parent IDs
+    TClonesArray *timeArray = wcsimrootevent->GetCherenkovHitTimes();
+    
+    int totalPe = 0;
+    // Loop through elements in the TClonesArray of WCSimRootCherenkovHits
+    for (i=0; i< ncherenkovhits; i++)
+    {
+      TObject *Hit = (wcsimrootevent->GetCherenkovHits())->At(i);
+      WCSimRootCherenkovHit *wcsimrootcherenkovhit = 
+	dynamic_cast<WCSimRootCherenkovHit*>(Hit);
 
-	TObject *element = (wcsimrootevent->GetCherenkovHits())->At(i);
-	
-	WCSimRootCherenkovHit *wcsimrootcherenkovhit = dynamic_cast<WCSimRootCherenkovHit*>(element);
-	int totpe = wcsimrootcherenkovhit->GetTotalPe();
-	printf("Total pe: %d\n",totpe);
-	int j;
-	for (j=0;j<(totpe<3 ? totpe : 3);j++)
+      int tubeNumber     = wcsimrootcherenkovhit->GetTubeID();
+      int timeArrayIndex = wcsimrootcherenkovhit->GetTotalPe(0);
+      int peForTube      = wcsimrootcherenkovhit->GetTotalPe(1);
+      totalPe += peForTube;
+     
+      if ( i < 10 ) // Only print first XX=10 tubes
+      {
+	printf("Total pe: %d times( ",peForTube);
+	for (int j = timeArrayIndex; j < timeArrayIndex + peForTube; j++)
 	{
-	  printf("Truetime: %d %f\n",j, wcsimrootcherenkovhit->GetTruetime(j));
+	  WCSimRootCherenkovHitTime HitTime = 
+	    dynamic_cast<WCSimRootCherenkovHitTime>(timeArray->At(j));
+	  
+	  printf("%6.2f ", HitTime->GetTruetime() );	     
 	}
-      } // End of loop over Cherenkov hits
-      */
-      // Look at digitized hit info
+	cout << ")" << endl;
+      }
 
-      // Get the number of digitized hits
-      /*
-      for (int index = 0 ; index < wcsimrootsuperevent->GetNumberOfEvents() ; 
-	   index++) {
+    } // End of loop over Cherenkov hits
+    cout << "Total Pe : " << totalPe << endl;
+    
+    // Look at digitized hit info
 
-	wcsimrootevent = wcsimrootsuperevent->GetTrigger(index);
-	cout << "Sub event number = " << index << "\n";
-	
-	int ncherenkovdigihits = wcsimrootevent->GetNcherenkovdigihits();
-	printf("Ncherenkovdigihits %d\n", ncherenkovdigihits);
-	
-	//	for (i=0;i<(ncherenkovdigihits>4 ? 4 : ncherenkovdigihits);i++){
-	for (i=0;i<ncherenkovdigihits;i++){
-	  
-	  // Loop through elements in the TClonesArray of WCSimRootCherenkovDigHits
-	  
-	  TObject *element = (wcsimrootevent->GetCherenkovDigiHits())->At(i);
-	  
-	  WCSimRootCherenkovDigiHit *wcsimrootcherenkovdigihit = dynamic_cast<WCSimRootCherenkovDigiHit*>(element);
-	  printf("q, t, tubeid: %f %f %d \n",wcsimrootcherenkovdigihit->GetQ(),
-		 wcsimrootcherenkovdigihit->GetT(),wcsimrootcherenkovdigihit->GetTubeId());
-	  
-	} // End of loop over Cherenkov digihits
-	
-      } // End of loop over trigger
-      */
-      // reinitialize super event between loops.
-      wcsimrootsuperevent->ReInitialize();
+    // Get the number of digitized hits
+    // Loop over sub events
+    cout << "DIGITIZED HITS:" << endl;
+    for (int index = 0 ; index < wcsimrootsuperevent->GetNumberOfEvents(); index++) 
+    {
+      wcsimrootevent = wcsimrootsuperevent->GetTrigger(index);
+      cout << "Sub event number = " << index << "\n";
       
-    } // End of loop over events
-	h1.Draw();
+      int ncherenkovdigihits = wcsimrootevent->GetNcherenkovdigihits();
+      printf("Ncherenkovdigihits %d\n", ncherenkovdigihits);
+      
+      //for (i=0;i<(ncherenkovdigihits>4 ? 4 : ncherenkovdigihits);i++){
+      for (i=0;i<ncherenkovdigihits;i++)
+      {
+    	// Loop through elements in the TClonesArray of WCSimRootCherenkovDigHits
+	
+    	TObject *element = (wcsimrootevent->GetCherenkovDigiHits())->At(i);
+	
+    	WCSimRootCherenkovDigiHit *wcsimrootcherenkovdigihit = 
+    	  dynamic_cast<WCSimRootCherenkovDigiHit*>(element);
+
+	if ( i < 10 ) // Only print first XX=10 tubes
+    	printf("q, t, tubeid: %f %f %d \n",wcsimrootcherenkovdigihit->GetQ(),
+    	       wcsimrootcherenkovdigihit->GetT(),wcsimrootcherenkovdigihit->GetTubeId());
+	
+      } // End of loop over Cherenkov digihits
+    } // End of loop over trigger
+    
+    // reinitialize super event between loops.
+    wcsimrootsuperevent->ReInitialize();
+    
+  } // End of loop over events
+  h1.Draw();
 }
