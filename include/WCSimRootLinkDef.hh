5a48968b3cb81311736342cbafbab76ac6f4347f
Matt Wetstein <mwetstein@hep.anl.gov>
2012-05-17 20:53:17 +0000

    include new WCSimRootCherenkovHitTime class in LinkDef

--- include/WCSimRootLinkDef.hh
+++ include/WCSimRootLinkDef.hh
@@ -6,7 +6,7 @@
 
 #pragma link C++ class WCSimRootCherenkovDigiHit+;
 #pragma link C++ class WCSimRootCherenkovHit+;
-#pragma link C++ class WCSimRootCherenkovHitTime+;
+#pragma link C++ class WCSimRootCherenkovHitCoordinate+;
 #pragma link C++ class WCSimRootTrack+;
 #pragma link C++ class WCSimRootEventHeader+;
 #pragma link C++ class WCSimRootTrigger+;
