170472e35bcbd96d3b9451fc038e54e17a592357
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2015-07-10 11:14:55 +0100

    Add a root exectuable that automatically loads the relevant WCSim libaries and includes. For running scripts in compiled mode

---
+++ rootwc/loadlibs.C
@@ -0,0 +1,22 @@
+{
+  TString libs0 = gSystem->GetDynamicPath();
+  TString libswc = gSystem->Getenv("WCSIMDIR");
+  TString libs  = libs0 + libswc + ":/usr/lib:/usr/local/lib:/opt/lib:/opt/local/lib";
+  gSystem->SetDynamicPath(libs.Data());
+
+  gSystem->Load("libGpad");
+  gSystem->Load("libPhysics");
+  gSystem->Load("libMatrix");
+  gSystem->Load("libHist");
+  gSystem->Load("libGraf");
+  gSystem->Load("libTree");
+  gSystem->Load("libRIO");
+  gSystem->Load("libXMLIO");
+  gSystem->Load("libMinuit");
+  //gSystem->Load("libMinuit2");
+  gSystem->Load("libMathMore"); 
+  
+  gSystem->Load("libWCSimRoot.so");
+  
+}
+
