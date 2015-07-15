c4e1e4aca0bde46ae57122360b2dc0506513f7b4
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2015-07-15 13:08:41 +0100

    Make analyse_dir.py more general, and use sample_readfile.C by default

--- verification-test-scripts/full_scripts_suite/analyse_dir.py
+++ verification-test-scripts/full_scripts_suite/analyse_dir.py
@@ -3,13 +3,12 @@
 import glob, os, argparse, sys
 
 parser = argparse.ArgumentParser(description='Analyse a directory of WCSim output files')
-parser.add_argument('-m','--macroname', type=str, default='$WCSIMDIR/verification-test-scripts/full_scripts_suite/daq_readfile.C', help='Default: $WCSIMDIR/verification-test-scripts/full_scripts_suite/daq_readfile.C')
-parser.add_argument('-d','--dir', type=str, help='Directory to analyse', default='./')
-parser.add_argument('-n','--nevents', type=int, help='Max number of events to analyse', default=9999999999)
+parser.add_argument('-c','--compiled-mode', action='store_true', help='Run in compiled mode? Requires rootwc in $PATH')
+parser.add_argument('-m','--macroname', type=str, default='$WCSIMDIR/sample-root-scripts/sample_readfile.C', help='Full path to the macro. Default: $WCSIMDIR/sample-root-scripts/sample_readfile.C')
+parser.add_argument('-d','--dir', type=str, help='Directory to analyse. Default .', default='./')
 parser.add_argument('-v','--verbose', action='store_true', help='Run verbosely?')
-parser.add_argument('-p','--pdfs', action='store_true', help='Create simple pdfs of (most of) the histograms created?')
-parser.add_argument('--histeveryevent', action='store_true', help='Make hit time histograms for every event? SLOW')
-parser.add_argument('-f','--onlyonefile', action='store_true', help='Run only one file')
+parser.add_argument('-f','--onlyonefile', action='store_true', help='Run only one file?')
+parser.add_argument('--additional-macro-options', type=str, default='', help='If your macro has more options than just filename & verbosity, specify the remaining arguments here. Recommended to wrap the command in \'\', then "" are dealt with correctly. WARNING: no whitespace allowed!')
 args = parser.parse_args()
 
 def py_bool_to_string(arg):
@@ -18,8 +17,6 @@ def py_bool_to_string(arg):
     else:
         return "false"
 verbose        = py_bool_to_string(args.verbose)
-histeveryevent = py_bool_to_string(args.histeveryevent)
-pdfs           = py_bool_to_string(args.pdfs)
 
 try:
     os.chdir(args.dir)
@@ -28,8 +25,15 @@ except:
     sys.exit(1)
 print "cd'ed to", args.dir
 
+prefix = 'root'
+if args.compiled_mode:
+    prefix = 'rootwc'
+prefix += " '" + args.macroname
+if args.compiled_mode:
+    prefix += '+'
+
 for file1 in glob.glob("wcsim_*.root"):
-    command = "rootwc '" + args.macroname + "+(\"" + file1 + "\"," + verbose + "," + str(args.nevents) + "," + pdfs + "," + histeveryevent + ")' -l -b -q"
+    command = prefix + '("' + file1 + '",' + verbose + (',' + args.additional_macro_options if args.additional_macro_options else '') + ")' -l -b -q"
     print command
     os.system(command)
     if args.onlyonefile:
