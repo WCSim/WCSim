7f79c0cc1790b87d01f4a531a38ba9b15afd28de
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2015-07-15 12:14:36 +0100

    generate_mac_files.py Set defaults, turn off new DAQ & dark noise options, make help messages clearer, new 'dry run' type option (generate files, but don't run wcsim)

--- verification-test-scripts/full_scripts_suite/generate_mac_files.py
+++ verification-test-scripts/full_scripts_suite/generate_mac_files.py
@@ -22,9 +22,12 @@ import shutil
 import sys
 import os
 
-delim_list = lambda s: s.split(',')
+delim_list = lambda s: list(set(s.split(',')))
 delim_list_str = lambda s: s.split(',') if len(s.split(',')) == 3 else s
 
+#DAQdigitizer_choices = ['SKIV', 'SKI', 'SKI_SKDETSIM']
+#DAQtrigger_choices = ['NHits', 'SKI_SKDETSIM', 'NHits2']
+#DAQtrigger_nhits_choices = ['NHits', 'SKI_SKDETSIM', 'NHits2']
 WCgeom_choices = ['HyperK', 'SuperK', \
                 'SuperK_20inchPMT_20perCent', \
                 'SuperK_20inchBandL_20perCent', \
@@ -40,41 +43,58 @@ WCgeom_choices = ['HyperK', 'SuperK', \
                 'DUSEL_200kton_10inch_HQE_12perCent', \
                 'DUSEL_200kton_12inch_HQE_10perCent', \
                 'DUSEL_200kton_12inch_HQE_14perCent' ]
+HKwatertargetlength_choices = ['HyperK', 'HyperK_withHPD']
 PMTQEMethod_choices = ['Stacking_Only', 'Stacking_And_SensitiveDetector', 'SensitiveDetector_Only']
-SavePi0_choices = ['true', 'false']
-DAQdigitizer_choices = ['SKIV', 'SKI', 'SKI_SKDETSIM']
-DAQtrigger_choices = ['NHits', 'SKI_SKDETSIM', 'NHits2']
+PMTCollEff_choices = ['on', 'off']
 GunPositionChoices = ['center', 'random', 'wall', 'minusx', 'plusx', 'minusz', 'plusz']
 GunDirectionChoices = ['towall', 'tocap', '4pi', 'wall']
+GunParticleChoices=['e-','e+','mu-','mu+','pi-','pi+','pi0','gamma','p+','n0']
+BatchChoices=['local','condor']
+
+def ListAsString(l):
+    return ' '.join(str(o)+',' for o in l)
 
 parser = argparse.ArgumentParser(description='Run many WCSim jobs with different options. Use , to delimit multiple options')
-parser.add_argument('--batchmode', type=str, required=True, choices=['local','condor'], help='Where to submit the jobs')
+#options about how to run this script
+parser.add_argument('--onlycreatefiles', action='store_true', help="Do a test run where you create all the files, but don't run WCSim?")
+parser.add_argument('--batchmode', type=str, required=True, choices=BatchChoices, help='Where to submit the jobs. Required')
 parser.add_argument('--vis', action='store_true', help='Turn on the visulation? Not yet implemented')
-parser.add_argument('--ReuseDAQFolder', action='store_true', help='Reuse the DAQ folders (i.e. don\'t exit if mkdir fails)')
-parser.add_argument('--WCgeom', type=delim_list, required=True, help='The water tank geometry')
-parser.add_argument('--HKwatertanklength', type=delim_list, required=True, help='The size of a HK geometry (mm)')
-parser.add_argument('--PMTQEMethod', type=delim_list, required=True, help='How the QE is applied')
-parser.add_argument('--SavePi0', type=delim_list, required=True, help='Save Pi0 info?')
-parser.add_argument('--DAQsavefailuresmode', type=delim_list, required=True, help='Save failed triggers mode')
-parser.add_argument('--DAQsavefailurestime', type=delim_list, required=True, help='Failed triggers trigger time')
-parser.add_argument('--DAQdigitizer', type=delim_list, required=True, help='Which digitizer class to use?')
-parser.add_argument('--DAQtrigger', type=delim_list, required=True, help='Which trigger class to use?')
-parser.add_argument('--DAQnhitsthreshold', type=delim_list, required=True, help='What value of the nhits trigger threshold should be used?')
-parser.add_argument('--DAQnhitswindow', type=delim_list, required=True, help='What value of the nhits trigger window should be used?')
+parser.add_argument('--reusedaqfolder', action='store_true', help='Reuse the DAQ folders? (i.e. don\'t exit if mkdir fails)')
+#options for the .mac files
+# geometry
+parser.add_argument('--WCgeom', type=delim_list, default='SuperK', help='The water tank geometry')
+parser.add_argument('--HKwatertanklength', type=delim_list, default='49500', help='The size of a HyperK geometry (mm)')
+"""
+# trigger & digitization
+parser.add_argument('--DAQsavefailuresmode', type=delim_list, default='0', help='Save failed triggers mode. 0: save only events which pass the trigger. 1: save only events which fail the trigger. 2: save both (default 0)')
+parser.add_argument('--DAQsavefailurestime', type=delim_list, default='200', help='For mode 1 & 2, give events which fail the trigger the trigger time (default 200)')
+parser.add_argument('--DAQdigitizer', type=delim_list, default='SKI', help='Which digitizer class to use? Specify multiple with comma separated list. Choices: '+ListAsString(DAQdigitizer_choices)+' (default SKI)')
+parser.add_argument('--DAQtrigger', type=delim_list, default='NHits', help='Which trigger class to use? Specify multiple with comma separated list. Choices: '+ListAsString(DAQtrigger_choices)+' (default NHits)')
+parser.add_argument('--DAQnhitsthreshold', type=delim_list, default='25', help='What value of the nhits trigger threshold should be used (i.e. number of hits/digits)? Specify multiple with comma separated list (default 25)')
+parser.add_argument('--DAQnhitswindow', type=delim_list, default='200', help='What value of the nhits trigger window should be used (ns)? Specify multiple with comma separated list (default 200)')
 parser.add_argument('--DAQnhitsignorenoise', action='store_true', help='Adjust the NHits threshold automatically for the dark noise rate?')
-parser.add_argument('--DarkNoiseRate', type=delim_list, required=True, help='Dark noise rate (kHz)')
-parser.add_argument('--DarkNoiseConvert', type=delim_list, required=True, help='Convert dark noise frequency before digitization to after digitization by setting suitable factor')
-parser.add_argument('--DarkNoiseMode', type=int, required=True, choices=[0,1], help='0: apply noise around hits. 1: apply noise in a specified time window')
-parser.add_argument('--DarkNoiseWindow', type=delim_list, required=True, help='Mode 0: Apply dark noise in a window around each hit. Mode 2: Apply dark noise in range x:y (use : as delimeter!)')
-parser.add_argument('--GunParticle', type=str, required=True, choices=['e-','e+','mu-','mu+','pi-','pi+','pi0','gamma'], help='Particle gun particle')
-parser.add_argument('--GunEnergy', type=delim_list, required=True, help='Particle gun energy (MeV)')
-parser.add_argument('--GunPosition', type=delim_list_str, required=True, help='Particle gun position')
-parser.add_argument('--GunDirection', type=delim_list_str, required=True, help='Particle gun direction')
-parser.add_argument('--NEvents', type=int, required=True, help='Number of events per configuration')
+"""
+# dark noise
+parser.add_argument('--DarkNoiseRate', type=delim_list, default='4.2', help='Dark noise rate (kHz). Specify multiple with comma separated list (default 4.2)')
+parser.add_argument('--DarkNoiseConvert', type=delim_list, default='1.367', help='Convert dark noise frequency before digitization to after digitization by setting suitable factor. Specify multiple with comma separated list (default 1.367)')
+#parser.add_argument('--DarkNoiseMode', type=int, default=1, choices=[0,1], help='0: apply noise in a specified time window. 1: apply noise around hits. Choose exactly one (default 1)')
+#parser.add_argument('--DarkNoiseWindow', type=delim_list, default='1500', help=' Mode 0: Apply dark noise in range x:y (use : as delimeter!). Mode 1: Apply dark noise in a window around each hit (+-window/2). Specify multiple (for the same mode) with a comma separated list (default 1500)')
+# pmt
+parser.add_argument('--PMTQEMethod', type=delim_list, default='Stacking_Only', help='How the QE is applied? Specify multiple with comma separated list. Choices: '+ListAsString(PMTQEMethod_choices)+' (default Stacking_Only)')
+parser.add_argument('--PMTCollEff', type=delim_list, default='off', help='Turn on/off the PMT collection efficiency? Specify multiple with comma separated list. Choices: '+ListAsString(PMTCollEff_choices)+' (default off)')
+# other
+parser.add_argument('--SavePi0', action='store_true', help='Save Pi0 info?')
+# particle gun
+parser.add_argument('--GunParticle', type=str, default='e-', choices=GunParticleChoices, help='Particle gun particle. Choose exactly one (default e-)')
+parser.add_argument('--GunEnergy', type=delim_list, default='500', help='Particle gun energy (MeV). Specify multiple with comma separated list (default 500)')
+parser.add_argument('--GunPosition', type=delim_list_str, default='0,0,0', help='Particle gun position. Either a comma-separated 3 vector OR exactly one of '+ListAsString(GunPositionChoices)+' (default 0,0,0)')
+parser.add_argument('--GunDirection', type=delim_list_str, default='1,0,0', help='Particle gun direction. Either a comma-separated 3 vector OR exactly one of '+ListAsString(GunDirectionChoices)+' (default 1,0,0)')
+parser.add_argument('--NEvents', type=int, default=10, help='Number of events per configuration (default 10)')
 
 args = parser.parse_args()
 
 def check_input_list(arglist, allowed, parser):
+    #check for options that aren't allowed
     for arg in arglist:
         if arg not in allowed:
             print arg, "is incorrect"
@@ -83,9 +103,9 @@ def check_input_list(arglist, allowed, parser):
             
 check_input_list(args.WCgeom, WCgeom_choices, parser)
 check_input_list(args.PMTQEMethod, PMTQEMethod_choices, parser)
-check_input_list(args.SavePi0, SavePi0_choices, parser)
-check_input_list(args.DAQdigitizer, DAQdigitizer_choices, parser)
-check_input_list(args.DAQtrigger, DAQtrigger_choices, parser)
+check_input_list(args.PMTCollEff, PMTCollEff_choices, parser)
+#check_input_list(args.DAQdigitizer, DAQdigitizer_choices, parser)
+#check_input_list(args.DAQtrigger, DAQtrigger_choices, parser)
 if (type(args.GunPosition) is list and len(args.GunPosition) != 3) \
     or (type(args.GunDirection) is list and len(args.GunDirection) != 3):
     print "GunPosition and GunDirection are three vectors. Specify exactly 3 options!"
@@ -106,6 +126,7 @@ if type(args.GunDirection) != type(args.GunPosition):
 
 origdir = os.getcwd()
 
+"""
 #make the jobOptions.mac file(s)
 DAQfolders = []
 for DAQdigitizer in args.DAQdigitizer:
@@ -129,7 +150,7 @@ for DAQdigitizer in args.DAQdigitizer:
                                 os.mkdir(foldername)
                             except OSError:
                                 print "Folder", foldername, "already exists"
-                                if not args.ReuseDAQFolder:
+                                if not args.reusedaqfolder:
                                     print "Exiting..."
                                     sys.exit(1)
                             os.chdir(foldername)
@@ -140,36 +161,41 @@ for DAQdigitizer in args.DAQdigitizer:
                                 "/DAQ/Digitizer " + DAQdigitizer + "\n" \
                                 "/DAQ/Trigger " + DAQtrigger + "\n" \
                                 "/DAQ/TriggerSaveFailures/Mode " + DAQsavefailuresmode + "\n" \
-                                "/DAQ/TriggerSaveFailures/TriggerTime " + DAQsavefailurestime + "\n" \
-                                "/DAQ/TriggerNHits/Threshold " + DAQnhitsthreshold + "\n" \
-                                "/DAQ/TriggerNHits/Window " + DAQnhitswindow + "\n" \
-                                "/DAQ/TriggerNHits/AdjustForNoise " + noise_agnostic + "\n"
+                                "/DAQ/TriggerSaveFailures/TriggerTime " + DAQsavefailurestime + "\n"
+                            if DAQtrigger in DAQtrigger_nhits_choices:
+                                text += "/DAQ/TriggerNHits/Threshold " + DAQnhitsthreshold + "\n" \
+                                    "/DAQ/TriggerNHits/Window " + DAQnhitswindow + "\n" \
+                                    "/DAQ/TriggerNHits/AdjustForNoise " + noise_agnostic + "\n"
                             #create the file
                             f = open('jobOptions.mac', 'w')
                             f.write(text)
                             f.close()
-                            #and don't forget the other .mac files
-                            shutil.copy2(os.path.expandvars("$WCSIMDIR") + "/jobOptions2.mac", "./")
-                            shutil.copy2(os.path.expandvars("$WCSIMDIR") + "/tuning_parameters.mac", "./")
-                            #and the exectuable
-                            if args.batchmode == 'condor':
-                                if not os.path.islink('WCSim'):
-                                    os.symlink(os.path.expandvars("$WCSIMDIR") + "/WCSim", "WCSim")
+"""
+
+#don't forget the other .mac files
+shutil.copy2(os.path.expandvars("$WCSIMDIR") + "/jobOptions.mac", "./")
+shutil.copy2(os.path.expandvars("$WCSIMDIR") + "/jobOptions2.mac", "./")
+shutil.copy2(os.path.expandvars("$WCSIMDIR") + "/tuning_parameters.mac", "./")
+#and the exectuable
+if args.batchmode == 'condor':
+    if not os.path.islink('WCSim'):
+        os.symlink(os.path.expandvars("$WCSIMDIR") + "/bin/Linux-g++/WCSim", "WCSim")
 
 #make the novis.mac analogue
 counter = 1
 for WCgeom in args.WCgeom:
     for HKwatertanklength in args.HKwatertanklength:
         for PMTQEMethod in args.PMTQEMethod:
-            for SavePi0 in args.SavePi0:
-                for DAQfolder in DAQfolders:
+            for PMTCollEff in args.PMTCollEff:
+                #for DAQfolder in DAQfolders:
                     for DarkNoiseRate in args.DarkNoiseRate:
                         for DarkNoiseConvert in args.DarkNoiseConvert:
-                            for DarkNoiseWindow in args.DarkNoiseWindow:
+                            #for DarkNoiseWindow in args.DarkNoiseWindow:
                                 for GunEnergy in args.GunEnergy:
+                                    """
                                     os.chdir(origdir + "/" + DAQfolder)
                                     #DarkNoiseWindow should be treated specially, as it depends on DarkNoiseMode
-                                    darknoisewindow = ""
+                                    darknoisewindow = 
                                     if args.DarkNoiseMode == 1:
                                         try:
                                             int(DarkNoiseWindow)
@@ -182,6 +208,7 @@ for WCgeom in args.WCgeom:
                                             "/DarkRate/SetDarkHigh " + DarkNoiseWindow.split(':')[1] + "\n"
                                     else:
                                         sys.exit(1)
+                                    """
 
                                     if type(args.GunPosition) is str:
                                         #if GunPosition and GunDirection are string's
@@ -206,26 +233,28 @@ for WCgeom in args.WCgeom:
                                             "/gun/direction " + " ".join(i for i in args.GunDirection) + "\n" \
                                             "/gun/position " + " ".join(i for i in args.GunPosition) + "\n"
                                         
-                                    #the WCSim/Construct line
+                                    #the detector construction options
                                     if WCgeom == 'SuperK':
-                                        constructoption = ''
+                                        constructoptions = ''
                                     else:
-                                        constructoption = "/WCSim/Construct \n"
+                                        constructoptions = "/WCSim/WCgeom " + WCgeom + "\n"
+                                        if WCgeom in HKwatertargetlength_choices:
+                                            constructoptions += "/WCSim/HyperK/waterTank_Length " + HKwatertanklength + "\n"
+                                        constructoptions += "/WCSim/Construct \n"
 
                                     #construct the filename
-                                    filenamestub = "wcsim_" + GunEnergy + args.GunParticle + "_" + WCgeom + "_" + HKwatertanklength + "_" + PMTQEMethod + "_SavePi0" + SavePi0 + "_DarkNoiseM" + str(args.DarkNoiseMode) + "R" + DarkNoiseRate + "W" + DarkNoiseWindow.strip()
+                                    #filenamestub = "wcsim_" + GunEnergy + args.GunParticle + "_" + WCgeom + "_" + (HKwatertanklength if (WCgeom in HKwatertargetlength_choices) else "") + "_" + PMTQEMethod + "_PMTCollEff_" + PMTCollEff + ("_SavePi0" if args.SavePi0 else "") + "_DarkNoiseM" + str(args.DarkNoiseMode) + "R" + DarkNoiseRate + "W" + DarkNoiseWindow.strip()
+                                    filenamestub = "wcsim_" + GunEnergy + args.GunParticle + "_" + WCgeom + "_" + (HKwatertanklength if (WCgeom in HKwatertargetlength_choices) else "") + "_" + PMTQEMethod + "_PMTCollEff_" + PMTCollEff + ("_SavePi0" if args.SavePi0 else "") + "_DarkNoise" + DarkNoiseRate
                                     #make the text for the config file
+                                    darknoisewindow = '' ####TODO remove this line when the new dark noise options go in
                                     text = "/run/verbose 0 \n" \
                                         "/tracking/verbose 0 \n" \
                                         "/hits/verbose 0 \n" \
-                                        "/WCSim/WCgeom " + WCgeom + "\n" \
-                                        "/WCSim/HyperK/waterTank_Length " + HKwatertanklength + "\n" \
-                                        "" + constructoption + "" \
+                                        "" + constructoptions + "" \
                                         "/WCSim/PMTQEMethod " + PMTQEMethod + "\n" \
-                                        "/WCSim/SavePi0 " + SavePi0 + "\n" \
+                                        "/WCSim/SavePi0 " + ("true" if args.SavePi0 else "false") + "\n" \
                                         "/DarkRate/SetDarkRate " + DarkNoiseRate + " kHz \n" \
                                         "/DarkRate/SetConvert " + DarkNoiseConvert + "\n" \
-                                        "/DarkRate/SetDarkMode " + str(args.DarkNoiseMode) + "\n" \
                                         "" + darknoisewindow + "" \
                                         "/mygen/generator normal " + "\n" \
                                         "" + gunoptions + "" \
@@ -265,6 +294,9 @@ for WCgeom in args.WCgeom:
                                         fcondor.close()
                                         command = 'condor_submit ' + filenamestub + '.jdl'
                                         
-                                    os.system(command)
                                     print command
+                                    if not args.onlycreatefiles:
+                                        os.system(command)
+                                    else:
+                                        print 'test run; not actually running WCSim'
                                     counter += 1
