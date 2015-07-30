43b2ac6cf1cda9ae5f7ec3156b12b79728e48f89
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2015-07-30 12:52:13 +0100

    Add doxygen comments to the new trigger classes

--- include/WCSimWCTriggerBase.hh
+++ include/WCSimWCTriggerBase.hh
@@ -1,3 +1,13 @@
+/**
+ * \class WCSimWCTriggerBase
+ *
+ * \brief The base class for WCSim triggering algorithms
+ *
+ * Concrete implementations of a trigger class should inherit from this class.
+ * Minimally, only DoTheWork() needs to be implemented in the implementation class.
+ *
+ */
+
 #ifndef WCSimWCTriggerBase_h
 #define WCSimWCTriggerBase_h 1
 
@@ -17,36 +27,72 @@ class WCSimWCTriggerBase : public G4VDigitizerModule
   
 public:
 
+  ///Create WCSimWCTriggerBase instance with knowledge of the detector and DAQ options
   WCSimWCTriggerBase(G4String name, WCSimDetectorConstruction*, WCSimWCDAQMessenger*);
-  ~WCSimWCTriggerBase();
   
-  //not recommended to override this method
-  virtual void Digitize(); //only defined virtual because it is overridden in the old class (WCSimWCDigitizer)
+  ~WCSimWCTriggerBase();
 
+  /**
+   * \brief The main user-callable routine of the class. Gets the input & creates the output WCSimWCDigitsCollection's, then calls DoTheWork()
+   *
+   * The virtual keyword for this method is DEPRECATED
+   * It is only defined virtual now because it is overridden in the old class (WCSimWCDigitizer)
+   */
+  virtual void Digitize();
+
+  ///Returns the number of trigger gates in the event (i.e. the number of triggers passed)
   int NumberOfGatesInThisEvent() { return TriggerTimes.size(); }
+  ///Get the time of the ith trigger
   Float_t              GetTriggerTime(int i) { return TriggerTimes[i];}
+  ///Get the trigger type of the ith trigger
   TriggerType_t        GetTriggerType(int i) { return TriggerTypes[i];}
+  ///Get the additional trigger information associated with the ith trigger
   std::vector<Float_t> GetTriggerInfo(int i) { return TriggerInfos[i];}
+
+  // Trigger algorithm option set methods
+  ///Set the threshold for the NHits trigger
   void SetNHitsThreshold(G4int threshold) { nhitsThreshold = threshold; }
+  ///Set the time window for the NHits trigger
   void SetNHitsWindow(G4int window) { nhitsWindow = window; }
-  virtual void SetPMTSize(G4float /*inputSize*/) {}; //function used in old class (WCSimWCDigitizer), called in WCSimEventAction
+
+  ///DEPRECATED function used in old class (WCSimWCDigitizer), and called in WCSimEventAction
+  virtual void SetPMTSize(G4float /*inputSize*/) {};
 
 protected:
 
-  virtual void DoTheWork(WCSimWCDigitsCollection* WCDCPMT) = 0; //this should call the trigger algorithms, and handle any temporary DigitsCollection's
+  ///This should call the trigger algorithms, and handle any temporary DigitsCollection's
+  virtual void DoTheWork(WCSimWCDigitsCollection* WCDCPMT) = 0;
+
   //these are the algorithms that perform triggering
+  //they are stored here so that different trigger classes can use the same algorithms without copying code
+  /**
+   * \brief An NHits trigger algorithm
+   *
+   * Looks through the input WCSimWCDigitsCollection and integrates the number of hits in a (specified) time window
+   * If the integral passes above a (specified) threshold, a trigger is issued
+   *
+   * The trigger type is kTriggerNHits
+   *
+   * The trigger time is the time of the first digit above threshold
+   *
+   * The trigger information is the number of hits in the time window (i.e. the number of hits that caused the trigger to fire)
+   *
+   * Currently setup with the optional 'test' argument which runs the algorithm with half the hit threshold
+   * for testing purposes. Triggers issued in this mode have type kTriggerNHitsTest
+   */
   void AlgNHits(WCSimWCDigitsCollection* WCDCPMT, bool remove_hits, bool test=false);
 
-  WCSimWCDigitsCollection*   DigitsCollection;
-  std::map<int,int>          DigiHitMap; // need to check if a hit already exists..
+  WCSimWCDigitsCollection*   DigitsCollection; ///< The main output of the class - collection of digits in the trigger window
+  std::map<int,int>          DigiHitMap; ///< Keeps track of the PMTs that have been added to the output WCSimWCDigitsCollection
 
-  std::vector<Float_t>                TriggerTimes;
-  std::vector<TriggerType_t>          TriggerTypes;
-  std::vector< std::vector<Float_t> > TriggerInfos;
+  std::vector<Float_t>                TriggerTimes; ///< The times of the triggers
+  std::vector<TriggerType_t>          TriggerTypes; ///< The type of the triggers
+  std::vector< std::vector<Float_t> > TriggerInfos; ///< Additional information associated with each trigger
 
-  WCSimWCDAQMessenger*       DAQMessenger;
-  WCSimDetectorConstruction* myDetector;
+  WCSimWCDAQMessenger*       DAQMessenger; ///< Get the options from the .mac file
+  WCSimDetectorConstruction* myDetector;   ///< Know about the detector, so can add appropriate PMT time smearing
 
+  /// Clear the Trigger* vectors and DigiHitMap
   void ReInitialize() {
     TriggerTimes.clear(); 
     TriggerTypes.clear(); 
@@ -56,16 +102,17 @@ protected:
 
 private:
 
-  G4int nhitsThreshold;
-  G4int nhitsWindow;
-
-  //takes all trigger times, then loops over all Digits & fills the output DigitsCollection
+  ///takes all trigger times, then loops over all Digits & fills the output DigitsCollection
   void FillDigitsCollection(WCSimWCDigitsCollection* WCDCPMT, bool remove_hits, TriggerType_t save_triggerType);
   
-  static const double offset; // hit time offset
-  static const double eventgateup; // ns
-  static const double eventgatedown; // ns
-  static const double LongTime; // ns
+  // Trigger algorithm options
+  G4int nhitsThreshold; ///< The threshold for the NHits trigger
+  G4int nhitsWindow;    ///< The time window for the NHits trigger
+
+  static const double offset;        ///< Hit time offset (ns)
+  static const double eventgateup;   ///< Digits are saved up to trigger time + eventgateup (ns)
+  static const double eventgatedown; ///< Digits are saved starting from trigger time - eventgatedown (ns)
+  static const double LongTime;      ///< An arbitrary long time to use in loops (ns)
 
 
 };
--- include/WCSimWCTriggerNHits.hh
+++ include/WCSimWCTriggerNHits.hh
@@ -1,3 +1,10 @@
+/**
+ * \class WCSimWCTriggerNHits
+ *
+ * \brief A simple NHits trigger class
+ *
+ */
+
 #ifndef WCSimWCTriggerNHits_h
 #define WCSimWCTriggerNHits_h 1
 
@@ -7,11 +14,13 @@ class WCSimWCTriggerNHits : public WCSimWCTriggerBase
 {
 public:
 
-  //not recommended to override these methods
+  ///Create WCSimWCTriggerNHits instance with knowledge of the detector and DAQ options
   WCSimWCTriggerNHits(G4String name, WCSimDetectorConstruction*, WCSimWCDAQMessenger*);
+
   ~WCSimWCTriggerNHits();
   
 private:
+  ///Calls the workhorse of this class: AlgNHits
   void DoTheWork(WCSimWCDigitsCollection* WCDCPMT);
 
 };
