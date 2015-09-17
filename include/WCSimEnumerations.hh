aa519a3a1e2a501c818dd381f120b732dffef8dd
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2015-09-17 09:55:47 +0100

    Add a TriggerTypeFromString() method

--- include/WCSimEnumerations.hh
+++ include/WCSimEnumerations.hh
@@ -2,6 +2,7 @@
 #define WCSimEnumerations_h 1
 
 #include <string>
+#include <iostream>
 
 typedef enum ETriggerType {
   kTriggerUndefined = -1,
@@ -54,6 +55,18 @@ public:
     }
     return "";
   }
+
+  static TriggerType_t TriggerTypeFromString(std::string s)
+  {
+    for(int i = int(kTriggerUndefined)+1; i <= kTriggerFailure; i++) {
+      if(s.compare(WCSimEnumerations::EnumAsString((TriggerType_t)i)) == 0) {
+	return (TriggerType_t)i;
+      }
+    }
+    std::cerr << "WCSimEnumerations::TriggerTypeFromString() Unknown string value " << s << std::endl;
+    return kTriggerUndefined;
+  }
+
 
 };
 
