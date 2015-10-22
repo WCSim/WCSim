2c8de55b134d3bee3d241dff73f08e728f2b6af5
Federico Nova <federico.nova@stfc.ac.uk>
2015-10-22 10:18:54 +0100

    add vector dictionary

--- sample-root-scripts/EventLinkDef.h
+++ sample-root-scripts/EventLinkDef.h
@@ -5,5 +5,11 @@
 #pragma link off all functions;
 
 #pragma link C++ class Event+;
+#pragma link C++ class vector<Float_t>+;
+#pragma link C++ class vector<Int_t>+;
+#pragma link C++ class vector< vector<Int_t> >+;
+#pragma link C++ class vector< vector< vector<Int_t> > >+;
+#pragma link C++ class vector< vector<Float_t> >+;
+#pragma link C++ class vector< vector< vector<Float_t> > >+;
 
 #endif
