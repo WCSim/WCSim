6397efd68a7191498c505821bcc8e9901fa58e3b
Tomoyo Yoshida <tyoshida@sukap0008.km.icrr.u-tokyo.ac.jp>
2016-04-25 14:16:35 +0900

    Add 5inch and 3inch PMTs as a copy of 8inch PMT

--- src/WCSimDetectorConstruction.cc
+++ src/WCSimDetectorConstruction.cc
@@ -278,6 +278,21 @@ WCSimPMTObject *WCSimDetectorConstruction::CreatePMTObject(G4String PMTType)
     WCSimDetectorConstruction::SetPMTPointer(PMT);
     return PMT;
   }
+  else if (PMTType == "PMT5inch"){
+    WCSimPMTObject* PMT = new PMT5inch;
+    WCSimDetectorConstruction::SetPMTPointer(PMT);
+    return PMT;
+  }
+  else if (PMTType == "PMT3inch"){
+    WCSimPMTObject* PMT = new PMT3inch;
+    WCSimDetectorConstruction::SetPMTPointer(PMT);
+    return PMT;
+  }
+  else if (PMTType == "PMT3inchGT"){
+    WCSimPMTObject* PMT = new PMT3inchGT;
+    WCSimDetectorConstruction::SetPMTPointer(PMT);
+    return PMT;
+  }
   else if (PMTType == "PMT10inch"){
     WCSimPMTObject* PMT = new PMT10inch;
     WCSimDetectorConstruction::SetPMTPointer(PMT);
