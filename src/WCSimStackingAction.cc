b9cacda749c4bb3148b672adaf8b6ed54a63a863
Stephane Zsoldos <stephane.zsoldos@gmail.com>
2018-01-17 16:18:35 +0000

    Remove OD part in StackingAction

--- src/WCSimStackingAction.cc
+++ src/WCSimStackingAction.cc
@@ -61,45 +61,7 @@ G4ClassificationOfNewTrack WCSimStackingAction::ClassifyNewTrack
 	    classification = fKill;
 	}
     }
-
-	G4String WCODCollectionName = "";
-
-	if(DetConstruct->GetIsODConstructed()){
-		WCODCollectionName = DetConstruct->GetODCollectionName();
-		// Make sure it is an optical photon
-		if( particleType == G4OpticalPhoton::OpticalPhotonDefinition() ){
-			// G4cout<<"entering OD in StackingAction....."<<G4endl;
-			G4float photonWavelength = (2.0*M_PI*197.3)/(aTrack->GetTotalEnergy()/CLHEP::eV);
-			G4float ratio = 1./(1.0-0.25);
-			G4float wavelengthQE = 0;
-			if(aTrack->GetCreatorProcess()==NULL) {
-				wavelengthQE  = DetConstruct->GetPMTQE(WCODCollectionName,photonWavelength,1,240,660,ratio);
-				if( G4UniformRand() > wavelengthQE ){
-					classification = fKill;
-					// G4cout<<"stage 0.."<<G4endl;
-				}
-			} else if (((G4VProcess*)(aTrack->GetCreatorProcess()))->GetProcessType()!=3) {
-				G4float photonWavelength = (2.0*M_PI*197.3)/(aTrack->GetTotalEnergy()/CLHEP::eV);
-				// MF : translated from skdetsim : better to increase the number of photons
-				// than to throw in a global factor  at Digitization time !
-				G4float ratio = 1./(1.0-0.25);
-				// XQ: get the maximum QE and multiply it by the ratio
-				// only work for the range between 240 nm and 660 nm for now
-				// Even with WLS
-				G4float wavelengthQE = 0;
-				if (DetConstruct->GetPMT_QE_Method()==1){ // G4cout<<"stage 1.."<<G4endl;
-					wavelengthQE  = DetConstruct->GetPMTQE(WCODCollectionName,photonWavelength,1,240,660,ratio);
-				}else if (DetConstruct->GetPMT_QE_Method()==2){  //G4cout<<"stage 2.."<<G4endl;
-					wavelengthQE  = DetConstruct->GetPMTQE(WCODCollectionName,photonWavelength,0,240,660,ratio);
-				}else if (DetConstruct->GetPMT_QE_Method()==3){ // G4cout<<"stage 3.."<<G4endl;
-					wavelengthQE = 1.1;
-				}
-
-				if( G4UniformRand() > wavelengthQE ) classification = fKill;
-			}
-		}
-	}//for OD
-
+  
   return classification;
 }
 
