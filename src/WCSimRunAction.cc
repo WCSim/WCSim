d683343a27bcc742a7e1346e21e4b1b74f3607c4
John Walker <jmgwalker@neut22.triumf.ca>
2019-04-11 17:03:19 -0700

    Fix offset bug.
    Need to remove additional offset before storing in settings tree.

--- src/WCSimRunAction.cc
+++ src/WCSimRunAction.cc
@@ -627,7 +627,7 @@ void WCSimRunAction::FillGeoTree(){
 
       if(fSettingsInputTree){
           fSettingsInputTree->GetEntry(0);
-          double z_offset = fNuPlanePos[2]/100.0 + fNuPrismRadius;
+          double z_offset = fNuPlanePos[2]/100.0;
           WCDetCentre[2] += z_offset;
           std::cout << "WCDetCentre[2] = " << WCDetCentre[2] << std::endl;
       }
