9b9a23d01516a92d61ccae9a10c5b89630ab8f3c
Guillaume Pronost <pronost@km.icrr.u-tokyo.ac.jp>
2020-12-02 22:44:30 +0900

    Add separate script to compile with ROOT5 or ROOT6 on sukap

---
+++ env_sukap_ROOT6.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+############################
+# ROOT/CERN
+############################
+# Note: official ROOT 5.34 installation on sukap is not compatible with cmake
+#  Temporary use Guillaume Pronost's version. Should change in the future.
+#
+#export ROOT_DIR=/disk02/usr6/pronost/software/root-5.34.38-build
+# Note: There is no official ROOT 6 installation 
+#  Temporary use Guillaume Pronost's version. Should change in the future.
+#  WARNING: This version is quite slow currently, investigation on-going
+export ROOT_DIR=/disk02/usr6/pronost/software/root6-build
+source ${ROOT_DIR}/bin/thisroot.sh
+
+alias root='root -l'
+
+############################
+# GEANT4
+############################
+
+pwd=$PWD
+cd /usr/local/geant4.10/geant4.10.03.p02/bin/
+source geant4.sh
+cd /usr/local/geant4.10/geant4.10.03.p02/share/Geant4-10.3.2/geant4make/
+source geant4make.sh
+echo "Connect a PATH of Geant4.10"
+cd "$pwd"
+
+############################
+# WCSim
+############################
+
+export WCSIMDIR=$(pwd)-build/ROOT6
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WCSIMDIR
+
