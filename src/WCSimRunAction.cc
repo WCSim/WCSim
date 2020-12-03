0d96a53b756e2244e118cbc083082734fbe96518
Nick Prouse <nprouse@triumf.ca>
2020-12-03 04:16:24 -0800

    Add git hash to the settings tree

--- src/WCSimRunAction.cc
+++ src/WCSimRunAction.cc
@@ -83,7 +83,10 @@ void WCSimRunAction::BeginOfRunAction(const G4Run* /*aRun*/)
     fSettingsOutputTree->Branch("WCDetCentre", WCDetCentre, "WCDetCentre[3]/D");
     fSettingsOutputTree->Branch("WCDetRadius", &WCDetRadius, "WCDetRadius/D");
     fSettingsOutputTree->Branch("WCDetHeight", &WCDetHeight, "WCDetHeight/D");
- 
+#ifdef GIT_HASH
+    const char* gitHash = GIT_HASH;
+    fSettingsOutputTree->Branch("GitHash", (void*)gitHash, "GetHash/C");
+#endif
   }      
 
   numberOfEventsGenerated = 0;
