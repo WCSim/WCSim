81aad8bad51cceed1e2c28d942aeb9788e07b787
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2021-04-14 10:53:39 +0100

    Add a robust way of knowing whether the user has set /DAQ/TriggerNDigits/PreTriggerWindow Don't put any restrictions on setting /DAQ/TriggerSaveFailures/PreTriggerWindow

--- src/WCSimWCDAQMessenger.cc
+++ src/WCSimWCDAQMessenger.cc
@@ -12,7 +12,7 @@
 #include <string>
 
 WCSimWCDAQMessenger::WCSimWCDAQMessenger(WCSimEventAction* eventaction) :
-  WCSimEvent(eventaction), StoreSaveFailuresPreWindow(-1E6), StoreNDigitsPreWindow(-1E6)
+  WCSimEvent(eventaction), NDigitsPreWindowSetByUser(false)
 {
   initialiseString = " (this is a default set; it may be overwritten by user commands)";
   initialised = false;
@@ -303,6 +303,7 @@ void WCSimWCDAQMessenger::SetNewValue(G4UIcommand* command,G4String newValue)
   else if (command == NDigitsPreTriggerWindow) {
     G4cout << "NDigits pretrigger window set to " << newValue << " ns" << initialiseString.c_str() << G4endl;
     StoreNDigitsPreWindow = NDigitsPreTriggerWindow->GetNewIntValue(newValue);
+    NDigitsPreWindowSetByUser = true;
   }
   else if (command == NDigitsPostTriggerWindow) {
     G4cout << "NDigits posttrigger window set to " << newValue << " ns" << initialiseString.c_str() << G4endl;
@@ -333,14 +334,10 @@ void WCSimWCDAQMessenger::SetTriggerOptions()
   G4cout << "\t" << failuremode << G4endl;
   WCSimTrigger->SetSaveFailuresTime(StoreSaveFailuresTime);
   G4cout << "\tTrigger time for events which fail all triggers will be set to " << StoreSaveFailuresTime << " ns" << G4endl;
-  if(StoreSaveFailuresPreWindow > -1E6) {
-    WCSimTrigger->SetSaveFailuresPreTriggerWindow(StoreSaveFailuresPreWindow);
-    G4cout << "\tSaveFailures pretrigger window set to " << StoreSaveFailuresPreWindow << " ns" << G4endl;
-  }
-  if(StoreSaveFailuresPostWindow >= 0) {
-    WCSimTrigger->SetSaveFailuresPostTriggerWindow(StoreSaveFailuresPostWindow);
-    G4cout << "\tSaveFailures posttrigger window set to " << StoreSaveFailuresPostWindow << " ns" << G4endl;
-  }
+  WCSimTrigger->SetSaveFailuresPreTriggerWindow(StoreSaveFailuresPreWindow);
+  G4cout << "\tSaveFailures pretrigger window set to " << StoreSaveFailuresPreWindow << " ns" << G4endl;
+  WCSimTrigger->SetSaveFailuresPostTriggerWindow(StoreSaveFailuresPostWindow);
+  G4cout << "\tSaveFailures posttrigger window set to " << StoreSaveFailuresPostWindow << " ns" << G4endl;
 
   if(StoreNDigitsThreshold >= 0) {
    WCSimTrigger->SetNDigitsThreshold(StoreNDigitsThreshold);
@@ -353,7 +350,7 @@ void WCSimWCDAQMessenger::SetTriggerOptions()
     WCSimTrigger->SetNDigitsWindow(StoreNDigitsWindow);
     G4cout << "\tNDigits trigger window set to " << StoreNDigitsWindow << " ns" << G4endl;
   }
-  if(StoreNDigitsPreWindow > -1E6) {
+  if(NDigitsPreWindowSetByUser) {
     WCSimTrigger->SetNDigitsPreTriggerWindow(StoreNDigitsPreWindow);
     G4cout << "\tNDigits pretrigger window set to " << StoreNDigitsPreWindow << " ns" << G4endl;
   }
