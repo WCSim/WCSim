cd360b13e59212b5a53851de8173b77ac64749a7
Mahdi Taani <22234246+mahditaani@users.noreply.github.com>
2021-09-09 08:47:43 +0100

    Commented out printout to reduce verbosity

--- src/WCSimEventAction.cc
+++ src/WCSimEventAction.cc
@@ -999,7 +999,7 @@ void WCSimEventAction::FillRootEvent(G4int event_id,
 	pdir[l]=mom[l];        // momentum-vector 
 	stop[l]=Stop[l]/cm; // stopping point 
 	start[l]=Start[l]/cm; // starting point 
-	G4cout<<"part 2 start["<<l<<"]: "<< start[l] <<G4endl;
+	//G4cout<<"part 2 start["<<l<<"]: "<< start[l] <<G4endl;
       }
 
 
