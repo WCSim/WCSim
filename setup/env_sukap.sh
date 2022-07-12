b0ca1f2618ff2af55c18f4cb8e7239f2f19ea9a9
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2022-07-12 23:29:47 +0900

    Account for new sukap locations

--- setup/env_sukap.sh
+++ setup/env_sukap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # ROOT version as argument, default value is 6 for ROOT6
-#ROOT_ARG=5
 ROOT_ARG=6
 
 if [ ! -z "$1" ] ; then
@@ -13,7 +12,7 @@ fi
 # CMAKE
 ############################
 
-export PATH=/usr/local/cmake-3.18.1/bin/:$PATH
+cmake --version
 
 ############################
 # ROOT/CERN
@@ -26,17 +25,19 @@ if [[ "$ROOT_ARG" == 5 ]] ; then
 	#
 	export ROOT_DIR=/home/pronost/software/root-5.34.38-build
 	export ROOT_STR=ROOT5
-else
+elif [[ "$ROOT_ARG" == 6 ]] ; then
 	echo "Use ROOT6"
 	# Note: There is no official ROOT 6 installation 
 	#  Temporary use Guillaume Pronost's version. Should change in the future.
 	export ROOT_DIR=/home/pronost/software/root-6.22.00-build
 	export ROOT_STR=ROOT6
+else
+    echo "Unknown argument: $1"
+    echo "Usage: env_sukap.sh [5|6]"
+    echo "Defaults to using ROOT 6"
+    return
 fi
 	
-# Note: There is no official ROOT 6 installation 
-#  Temporary use Guillaume Pronost's version. Should change in the future.
-#export ROOT_DIR=/home/pronost/software/root-6.22.00-build
 source ${ROOT_DIR}/bin/thisroot.sh
 
 alias root='root -l'
@@ -46,17 +47,20 @@ alias root='root -l'
 ############################
 
 pwd=$PWD
-cd /usr/local/geant4.10/geant4.10.03.p02/bin/
+GEANT4VERSION=geant4.10.05.p01
+#alternative naming convention used by the share/ folder...
+GEANT4VERSION_ALT=Geant4-10.5.1
+cd /usr/local/sklib_gcc8/$GEANT4VERSION/bin/
 source geant4.sh
-cd /usr/local/geant4.10/geant4.10.03.p02/share/Geant4-10.3.2/geant4make/
+cd /usr/local/sklib_gcc8/$GEANT4VERSION/share/$GEANT4VERSION_ALT/geant4make/
 source geant4make.sh
-echo "Connect a PATH of Geant4.10"
+echo "Using $GEANT4VERSION"
 cd "$pwd"
 
 ############################
 # WCSim
 ############################
 export BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-export WCSIMDIR=$(pwd)-build/ROOT5/$BRANCH_NAME
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WCSIMDIR
-
+export WCSIMDIR=$(pwd)-build/$ROOT_STR/$BRANCH_NAME
+export LD_LIBRARY_PATH=$WCSIMDIR:$LD_LIBRARY_PATH
+export WCSIMREPO=$(pwd)
