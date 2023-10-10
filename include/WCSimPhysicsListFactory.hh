1caad91d3466390c61e4491c3cf6735823f50a2f
Jack Fannon <jackepfannon@gmail.com>
2023-10-10 14:31:54 +0100

    Revert "Reformatting"
    This reverts commit 9c7233cde083afab4e54a76c26568b30270283cd.

--- include/WCSimPhysicsListFactory.hh
+++ include/WCSimPhysicsListFactory.hh
@@ -1,43 +1,46 @@
 #ifndef WCSimPhysicsListFactory_h
 #define WCSimPhysicsListFactory_h 1
 
-#include "G4OpticalPhysics.hh"
-#include "G4PhysListFactory.hh"
-#include "G4RadioactiveDecayPhysics.hh"
-#include "G4UnitsTable.hh"
-#include "G4VModularPhysicsList.hh"
 #include "globals.hh"
+#include "G4VModularPhysicsList.hh"
+#include "G4PhysListFactory.hh"
+#include "G4UnitsTable.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4RadioactiveDecayPhysics.hh" 
 
 #include "WCSimPhysicsListFactoryMessenger.hh"
 #include "WCSimRootOptions.hh"
 
-// class WCSimPhysicsList;
+//class WCSimPhysicsList;
 
-class WCSimPhysicsListFactory : public G4VModularPhysicsList {
-public:
-  WCSimPhysicsListFactory();
-  ~WCSimPhysicsListFactory();
+class WCSimPhysicsListFactory : public G4VModularPhysicsList
+{
+  public:
+    WCSimPhysicsListFactory();
+    ~WCSimPhysicsListFactory();
 
-  void SetList(G4String newvalue);       // called by messenger
-  void SetnCaptModel(G4String newvalue); // called by messenger
-  void InitializeList();
+    void SetList(G4String newvalue);  // called by messenger
+    void SetnCaptModel(G4String newvalue);  // called by messenger
+    void InitializeList();
 
-  // G4String GetPhysicsListName() {return PhysicsListName;}
+    //G4String GetPhysicsListName() {return PhysicsListName;}
 
-  void ConstructParticle();
-  void ConstructProcess();
-  void SetCuts();
+    void ConstructParticle();
+    void ConstructProcess();
+    void SetCuts();
 
-  void SaveOptionsToOutput(WCSimRootOptions *wcopt);
+    void SaveOptionsToOutput(WCSimRootOptions * wcopt);
 
-private:
-  G4String PhysicsListName;
-  G4String ValidListsString;
+  private:
 
-  G4String nCaptModelChoice;
+    G4String PhysicsListName;
+    G4String ValidListsString;
+    
+    G4String nCaptModelChoice;
 
-  WCSimPhysicsListFactoryMessenger *PhysicsMessenger;
-  G4PhysListFactory *factory;
+    WCSimPhysicsListFactoryMessenger* PhysicsMessenger;
+    G4PhysListFactory* factory;
+
 };
 
 #endif
