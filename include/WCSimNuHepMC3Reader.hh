4b221d43d7c3a2f2cea54d13b24b761d51f6c5f7
Jack Fannon <jackepfannon@gmail.com>
2023-11-29 15:31:40 +0000

    Added read event and hepmc reader

--- include/WCSimNuHepMC3Reader.hh
+++ include/WCSimNuHepMC3Reader.hh
@@ -1,7 +1,14 @@
 #include <G4String.hh>
 
+#include "HepMC3/ReaderFactory.h"
+
 class WCSimNuHepMC3Reader {
   public:
     WCSimNuHepMC3Reader(G4String filename);
     ~WCSimNuHepMC3Reader();
+
+    int ReadEvent(HepMC3::GenEvent &evt);
+
+  private:
+    std::shared_ptr<HepMC3::Reader> hepmc_reader;
 };
--- src/WCSimNuHepMC3Reader.cc
+++ src/WCSimNuHepMC3Reader.cc
@@ -1,9 +1,19 @@
 #include "WCSimNuHepMC3Reader.hh"
 #include <G4ios.hh>
-
-#include "HepMC3/HepMC3.h"
-#include "HepMC3/Reader.h"
+#include <cstdlib>
 
 WCSimNuHepMC3Reader::WCSimNuHepMC3Reader(G4String filename) {
     G4cout << "WCSimNuHepMC3Reader::WCSimNuHepMC3Reader(G4String filename) called " << filename << G4endl;
+
+    hepmc_reader = HepMC3::deduce_reader(filename);
+    if (!hepmc_reader) {
+        G4cout << "NuHepMC3Reader: \033[31m[ERROR]\033[0m Failed to determine input type for HepMC3 file: "
+               << filename << G4endl;
+        exit(-1);
+    }
+}
+
+int WCSimNuHepMC3Reader::ReadEvent(HepMC3::GenEvent &event) {
+    hepmc_reader->read_event(event);
+    return 1;
 }
