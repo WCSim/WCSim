d07295840a2509ac0c2e088d895e54c2c6228a18
patrickstowell <p.stowell@sheffield.ac.uk>
2025-02-22 17:37:20 +0000

    Added missing example

---
+++ python/addkernel.sh
@@ -0,0 +1,3 @@
+# To register your current python3 virtualenv kernel with one that matches
+# the precompiled ROOT version use the value below before starting jupyter.
+python3.9 -m ipykernel install --name 'python3.9-g4'
