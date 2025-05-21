71f8bbd96e845f029f26ad362ca3815e41db385f
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2025-05-21 14:33:43 +0100

    Add ability to store BoundaryWall dimensions

--- include/WCSimDetectorConstruction.hh
+++ include/WCSimDetectorConstruction.hh
@@ -496,6 +496,8 @@ public:
   ////////// END OD /////////////
   ///////////////////////////////
 
+  std::map<BoundaryWallType_t, std::vector<G4float> > GetBoundaryWallDimensions() const {return fBoundaryWallDimensions;}
+
 private:
 
   // Overlap checking turned on and off with cmake option
@@ -650,6 +652,22 @@ private:
   G4String Vis_Choice;
   
 
+  /// Boundary wall (e.g. blacksheet/tyveks) dimensions
+  std::map<BoundaryWallType_t, std::vector<G4float> > fBoundaryWallDimensions;
+
+  void AddBoundaryWallCylinderDimensions(BoundaryWallType_t s,
+					 G4float radius,
+					 G4float full_length) {
+    auto it = fBoundaryWallDimensions.find(s);
+    if(it != fBoundaryWallDimensions.end()) {
+      G4cerr << "Boundary wall type " << WCSimEnumerations::EnumAsString(s)
+	     << " already exists inside fBoundaryWallDimensions. Replacing dimensions"
+	     << G4endl;
+    }
+    std::vector<G4float> dims = {radius, full_length};
+    fBoundaryWallDimensions[s] = dims;
+  }
+  
   G4double WCLength;
 
   G4double WCPosition;
