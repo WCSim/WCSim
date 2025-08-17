9fe182d671673cf6848cdf2536ce116c4e9397ca
Arturo Fiorentini <arturofiorentini@cunet.carleton.ca>
2025-08-16 21:35:11 -0400

    Update include/WCSimOpRaman.hh
    Co-authored-by: Tom Dealtry <tom.deal135@gmail.com>

--- include/WCSimOpRaman.hh
+++ include/WCSimOpRaman.hh
@@ -1,51 +1,6 @@
 // Modified copy of G4OpRayleigh.cc of geant4.10 to implement Raman scattering
 // Reference: APPLIED OPTICS / Vol. 37, No. 15 / 20 May 1998
 //
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-// $Id: G4OpRayleigh.hh 84596 2014-10-17 07:34:41Z gcosmo $
-//
-// 
-////////////////////////////////////////////////////////////////////////
-// Optical Photon Rayleigh Scattering Class Definition
-////////////////////////////////////////////////////////////////////////
-//
-// File:        G4OpRayleigh.hh
-// Description: Discrete Process -- Rayleigh scattering of optical photons 
-// Version:     1.0
-// Created:     1996-05-31
-// Author:      Juliet Armstrong
-// Updated:     2014-08-20 allow for more material types
-//              2005-07-28 add G4ProcessType to constructor
-//              1999-10-29 add method and class descriptors
-//              1997-04-09 by Peter Gumplinger
-//              > new physics/tracking scheme
-// mail:        gum@triumf.ca
-//
-////////////////////////////////////////////////////////////////////////
 
 #ifndef WCSimOpRaman_h
 #define WCSimOpRaman_h 1
