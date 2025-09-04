564cb5c2d3ebdd9706ca8b8bb48454b32bee906c
federiconova <federico.nova@stfc.ac.uk>
2025-09-04 14:46:36 +0100

    define eljen/kuraray choice in Build.h

---
+++ include/Build.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <string>
+extern const std::string HKFD_OD_WLS_PLATE_TYPE;  // Declaration only
---
+++ src/Build.cc
@@ -0,0 +1,3 @@
+#include "Build.h"
+//const std::string HKFD_OD_WLS_PLATE_TYPE = "EljenEJ286";  // Definition
+const std::string HKFD_OD_WLS_PLATE_TYPE = "Kuraray";  // Definition
