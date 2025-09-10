4252c82dcdf25c0eeefc8b8de6329b310fe09c1c
federiconova <federico.nova@stfc.ac.uk>
2025-09-10 20:31:53 +0100

    set INR as standard plate for FDOD; extend INR emission spectrum at large wavelength

--- src/Build.cc
+++ src/Build.cc
@@ -1,8 +1,8 @@
 #include "Build.h"
 // define type for FD OD WLS plate
 //const std::string HKFD_OD_WLS_PLATE_TYPE = "EljenEJ286";  // Definition
-const std::string HKFD_OD_WLS_PLATE_TYPE = "Kuraray";  // Definition
-//const std::string HKFD_OD_WLS_PLATE_TYPE = "Inr";  // Definition
+//const std::string HKFD_OD_WLS_PLATE_TYPE = "Kuraray";  // Definition
+const std::string HKFD_OD_WLS_PLATE_TYPE = "Inr";  // Definition
 
 // define type for IWCD WLS plate
 const std::string IWCD_OD_WLS_PLATE_TYPE = "EljenEJ286";  // Definition
